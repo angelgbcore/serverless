fdd7aabde91e4704f2e6af8449d2c2c288637134
AngelBarrera12 <barreraosorioangelgabriel@gmail.com>
2021-11-24 19:30:46 -0600

    subida de archivos

--- 02api-flask/usarMongoDB.py
+++ 02api-flask/usarMongoDB.py
@@ -1,16 +1,6 @@
 from pymongo import MongoClient
 import json
 
-# establish connex
-conn = MongoClient('localhost', 27017)
-conn = MongoClient()
-print ('conectado...')
-
-# create db
-db = conn.baseDeDatos
-
-# create collection
-collection = db.InfoDeNotebooks
 
 # <-------------------------------------------------------- INSERT 
 # Funciones para insertar datos
@@ -19,4 +9,15 @@ def insertDatosNotebook(NBDataconFecha) :
     Una vez que se agrego fecha y hora en la NBDataconFecha recibida por POST
     agregamos el documento "registro"  en la base de datos.
     """
+    # establish connex
+    conn = MongoClient('localhost', 27017)
+    conn = MongoClient()
+    print ('conectado...')
+
+    # create db
+    db = conn.baseDeDatos
+
+    # create collection
+    collection = db.InfoDeNotebooks
+
     insertNotebook = collection.insert(NBDataconFecha)
